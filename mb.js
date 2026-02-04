7306f0a6e0e4becb437a8fc993ada2308acba8ff
lz19970619 <1914549823@proton.me>
2026-02-04 16:14:33 +0800

    mb.js

--- mb.js
+++ mb.js
@@ -1,5 +1,3 @@
-// 苹果cms必备
-var maccms={"path":"__ROOT__","mid":"{$maccms['mid']}","url":"{$maccms['site_url']}","wapurl":"{$maccms['site_wapurl']}","mob_status":"{$maccms['mob_status']}"};
 
 
 // 返回顶部
